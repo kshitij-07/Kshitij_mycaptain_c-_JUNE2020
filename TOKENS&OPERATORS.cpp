38b2d67b8c1850eb4ae1519112b0bf9248222594
kshitij-07 <66638543+kshitij-07@users.noreply.github.com>
2020-06-12 18:15:27 +0530

    Create TOKENS&OPERATORS.cpp
    This file prints the size of datatypes such as int, float, double and char.

---
+++ TOKENS&OPERATORS.cpp
@@ -0,0 +1,12 @@
+/*Q1  Write a c++ program to find size of int, float, double and char in your system
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"The size of datatype int is:"<<sizeof(int)<<endl;
+	cout<<"The size of datatype float is:"<<sizeof(float)<<endl;
+	cout<<"The size of datatype double is:"<<sizeof(double)<<endl;
+	cout<<"The size of datatype char is:"<<sizeof(char)<<endl;
+	return 0;
+}
